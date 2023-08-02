ec63dfb9cf3355a8a853d69ffe0a6fc37fd04681
John <MrGriboman@eduard.net.kht.ru>
2023-08-02 19:42:09 +1000

    populate the board

--- Board.mjs
+++ Board.mjs
@@ -15,6 +15,7 @@ export class Board {
         w /= nCol;            // width of a block
         h /= nRow;            // height of a block
 
+        ctx.fillStyle = "#CC9966";
         for (let i = 0; i < nRow; ++i) {
             for (let j = 0, col = nCol / 2; j < col; ++j) {
                 ctx.rect(2 * j * w + (i % 2 ? 0 : w), i * h, w, h);
@@ -25,7 +26,21 @@ export class Board {
     }
 
     populate(can) {
-        const checker = new Checker(2, 5, Colors.WHITE, this.block_size);
-        checker.draw(can)
+        for (let i = 0; i < 3; ++i) {
+            for (let j = 0; j < 8; ++j) {
+                if ((i + j) % 2 != 0) {
+                    let checker = new Checker(j, i, Colors.BLACK, this.block_size);
+                    checker.draw(can);
+                }
+            }
+        }
+        for (let i = 5; i < 8; ++i) {
+            for (let j = 0; j < 8; ++j) {
+                if ((i + j) % 2 != 0) {
+                    let checker = new Checker(j, i, Colors.WHITE, this.block_size);
+                    checker.draw(can);
+                }
+            }
+        }
     }
 }
--- checker.mjs
+++ checker.mjs
@@ -20,7 +20,7 @@ export class Checker {
     draw(canvas) {
         const ctx = canvas.getContext('2d');
         const block_size = canvas.width / 8;
-        ctx.fillStyle = '#FFFFFF';
+        ctx.fillStyle = (this.color === Colors.BLACK) ? "#000000" : "#FFFFFF";
         ctx.beginPath();
         ctx.arc(this.x + block_size / 2, this.y + block_size / 2, block_size / 2 - 10, 0, 2 * Math.PI);
         ctx.fill();
